d8ccd78c28df61542681a27bcc17a2894cfc9a62
Marcus <marcus.c.eliasson@gmail.com>
2025-11-19 14:27:49 +0100

    Fix issue of RNE/TXE and ADDR flag being set at the same time

--- Core/Src/stm32c0xx_it.c
+++ Core/Src/stm32c0xx_it.c
@@ -168,42 +168,7 @@ void SysTick_Handler(void)
 void I2C1_IRQHandler(void)
 {
   /* USER CODE BEGIN I2C1_IRQn 0 */
-  if (LL_I2C_IsActiveFlag_ADDR(I2C1))
-  {
-    if ((uint8_t)LL_I2C_GetAddressMatchCode(I2C1) == memory_get_i2c_address() << 1)
-    {
-      DBG_PRINT("Got hit on real address (0x%02X)\n", LL_I2C_GetAddressMatchCode(I2C1) >> 1);
-      // For every transaction on the bus this is the first thing which happens.
-      // Both when reading and writing the first action is to write the 16-bit
-      // register address.
-      if (LL_I2C_GetTransferDirection(I2C1) == LL_I2C_DIRECTION_WRITE)
-      {
-        state = I2C_STATE_ADDR_1;
-      }
-      else
-      {
-        state = I2C_STATE_READ;
-
-        LL_I2C_EnableIT_TX(I2C1);
-      }
-    }
-    else if (LL_I2C_GetAddressMatchCode(I2C1) == I2C_ADDR_RESTART_DISCOVERY) {
-      DBG_PRINT("Read on restart discovery address, reset to discovery address on STOP\n");
-      state = I2C_STATE_RESET_TO_DISCOVERY_ON_STOP;
-      LL_I2C_EnableIT_TX(I2C1);
-    }
-    else if (LL_I2C_GetAddressMatchCode(I2C1) == I2C_ADDR_RESET) {
-      DBG_PRINT("Read on reset address, reset to discovery address on STOP\n");
-      state = I2C_STATE_WAITING_TO_RESET_SYSTEM;
-      LL_I2C_EnableIT_TX(I2C1);
-      memory_set_i2c_address(I2C_ADDR_DISCOVERY >> 1); // Reset the address to discovery
-    }
-    else
-    {
-      DBG_PRINT("Not my address: 0x%02X\n", LL_I2C_GetAddressMatchCode(I2C1) >> 1);
-    }
-    LL_I2C_ClearFlag_ADDR(I2C1);
-  } else if (LL_I2C_IsActiveFlag_RXNE(I2C1)) {
+  if (LL_I2C_IsActiveFlag_RXNE(I2C1)) {
     uint8_t data = LL_I2C_ReceiveData8(I2C1);
 
     switch (state) {
@@ -239,6 +204,42 @@ void I2C1_IRQHandler(void)
       uint8_t data = memory_read(reg_address++);
       LL_I2C_TransmitData8(I2C1, data);
     }
+  }
+  else if (LL_I2C_IsActiveFlag_ADDR(I2C1))
+  {
+    if ((uint8_t)LL_I2C_GetAddressMatchCode(I2C1) == memory_get_i2c_address() << 1)
+    {
+      DBG_PRINT("Got hit on real address (0x%02X)\n", LL_I2C_GetAddressMatchCode(I2C1) >> 1);
+      // For every transaction on the bus this is the first thing which happens.
+      // Both when reading and writing the first action is to write the 16-bit
+      // register address.
+      if (LL_I2C_GetTransferDirection(I2C1) == LL_I2C_DIRECTION_WRITE)
+      {
+        state = I2C_STATE_ADDR_1;
+      }
+      else
+      {
+        state = I2C_STATE_READ;
+
+        LL_I2C_EnableIT_TX(I2C1);
+      }
+    }
+    else if (LL_I2C_GetAddressMatchCode(I2C1) == I2C_ADDR_RESTART_DISCOVERY) {
+      DBG_PRINT("Read on restart discovery address, reset to discovery address on STOP\n");
+      state = I2C_STATE_RESET_TO_DISCOVERY_ON_STOP;
+      LL_I2C_EnableIT_TX(I2C1);
+    }
+    else if (LL_I2C_GetAddressMatchCode(I2C1) == I2C_ADDR_RESET) {
+      DBG_PRINT("Read on reset address, reset to discovery address on STOP\n");
+      state = I2C_STATE_WAITING_TO_RESET_SYSTEM;
+      LL_I2C_EnableIT_TX(I2C1);
+      memory_set_i2c_address(I2C_ADDR_DISCOVERY >> 1); // Reset the address to discovery
+    }
+    else
+    {
+      DBG_PRINT("Not my address: 0x%02X\n", LL_I2C_GetAddressMatchCode(I2C1) >> 1);
+    }
+    LL_I2C_ClearFlag_ADDR(I2C1);
   }
 
   if (LL_I2C_IsActiveFlag_ARLO(I2C1)) {
